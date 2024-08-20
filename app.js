d3fe125f52e6e12b8227a1c027bf1efb0b456553
pawansuthar01 <paw.kum.2111@gmail.com>
2024-08-20 16:15:10 +0530

    modi. file add

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ display2.value = "";
 
 function appendnum(number) {
   currvalue += number;
-  display.value = currvalue;
+  display.innerText = currvalue;
   block = true;
   count = 1;
 }
@@ -18,38 +18,46 @@ function appendop(op) {
     block = false;
     count = 0;
     currvalue += "" + op + "";
-    display.value = currvalue;
+    display.innerText = currvalue;
   }
 }
 function calculator() {
   if (count !== 0) {
     try {
       let result = eval(currvalue);
-      display2.value = result;
+      display2.innerText = result;
+      display.classList.add("active");
+      display2.classList.remove("hidden");
     } catch (error) {
-      display2.value = error;
+      display2.innerText = error;
+      display.classList.add("active");
+      display2.classList.remove("hidden");
     }
   } else {
     if (currvalue != 0) {
       try {
-        let arr = display.value;
+        let arr = display.innerText;
         let newotal = arr.substring(0, arr.length - 1);
-        display2.value = eval(newotal);
+        display2.innerText = eval(newotal);
+        display.classList.add("active");
+        display2.classList.remove("hidden");
       } catch (error) {
-        display2.value = error;
+        display2.innerText = error;
       }
     }
   }
 }
 function cler() {
   let newc = currvalue.substring(0, currvalue.length - 1);
-  display.value = newc;
+  display.innerText = newc;
   currvalue = newc;
   count = 0;
 }
 function clerall() {
-  display.value = "";
+  display.innerText = "";
   currvalue = "";
-  display2.value = "";
+  display.classList.remove("active");
+  display2.classList.add("hidden");
+  display2.innerText = "";
   count = 0;
 }
